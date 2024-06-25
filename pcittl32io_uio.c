0a0829d622e013efb0a0b08b4b86437ecf4c2023
Johannes Roith <johannes@gnu-linux.rocks>
2024-06-25 22:04:39 +0200

    Memory Mapping and example program added

--- pcittl32io_uio.c
+++ pcittl32io_uio.c
@@ -16,6 +16,8 @@ static int pcittl32io_probe(struct pci_dev *pdev, const struct pci_device_id *id
 {
 	int status;
 	struct uio_info *info;
+	resource_size_t start = pci_resource_start(pdev, 0);
+	resource_size_t len = pci_resource_len(pdev, 0);
 
 	dev_info(&pdev->dev, "Probing...\n");
 
@@ -35,16 +37,32 @@ static int pcittl32io_probe(struct pci_dev *pdev, const struct pci_device_id *id
 	info->name = "pcittl32io";
 	info->version = "V1.0";
 
+	/* Let's setup memory entry for BAR0 */
+	info->mem[0].name = "pcittl32io_bar0";
+	info->mem[0].addr = start & PAGE_MASK;
+	info->mem[0].offs = start & ~PAGE_MASK;
+	info->mem[0].size = ((start & ~PAGE_MASK) + len + PAGE_SIZE -1) & PAGE_MASK;
+	info->mem[0].memtype = UIO_MEM_PHYS;
+	info->mem[0].internal_addr = pci_ioremap_bar(pdev, 0);
+
+	if (!info->mem[0].internal_addr) {
+		dev_err(&pdev->dev, "Error! No memory behind BAR0\n");
+		status = -ENODEV;
+		goto free_region;
+	}
+
+
 	status = uio_register_device(&pdev->dev, info);
 	if (status) {
 		dev_err(&pdev->dev, "Error registering UIO device\n");
-		goto free_region;
+		goto free_mem;
 	}
 
 	pci_set_drvdata(pdev, info);
 	
 	return 0;
-
+free_mem:
+	iounmap(info->mem[0].internal_addr);
 free_region:
 	pci_release_regions(pdev);
 free_dev:
@@ -60,6 +78,7 @@ static void pcittl32io_remove(struct pci_dev *pdev)
 
 	info = pci_get_drvdata(pdev);
 
+	iounmap(info->mem[0].internal_addr);
 	uio_unregister_device(info);
 
 	pci_release_regions(pdev);
